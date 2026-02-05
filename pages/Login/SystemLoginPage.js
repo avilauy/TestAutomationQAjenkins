bd07e785b72594a9d2420ea46a3e8b4d22999b3c
avilauy <ivan-avila@resonet.uy>
2026-02-05 14:26:35 -0300

    tets

--- pages/Login/SystemLoginPage.js
+++ pages/Login/SystemLoginPage.js
@@ -22,33 +22,18 @@ class SystemLoginPage {
     await this.page.locator(this.loginButton).waitFor({ state: 'visible', timeout: 15000 });
     await this.page.locator(this.loginButton).click();
   }
-
- /// async esperarLoginExitoso() {
- ///   // Aquí está la clave:
- ///   // esperamos el DOM del HOME, no la URL
- ///   await this.page.locator(this.selectorHome).waitFor({
- ///     state: 'visible',
- ///     timeout: 60000
- ///   });
- ///   await this.page.screenshot({ path: 'allure-results/login-fail.png', fullPage: true });
- /// }
 async esperarLoginExitoso() {
-  await this.page.waitForLoadState('networkidle', { timeout: 60000 });
+  // 1️⃣ esperar navegación real
+  //await this.page.waitForURL('**/Menus/**', { timeout: 60000 });
 
-  const home = this.page.locator(this.selectorHome);
-  const error = this.page.locator(
-    '.alert-danger, .error, [data-testid="login-error"]'
-  );
-
-  await Promise.race([
-    home.waitFor({ state: 'visible', timeout: 60000 }),
-    error.waitFor({ state: 'visible', timeout: 60000 })
-  ]);
-
-  if (await error.isVisible()) {
-    throw new Error('Login fallido: mensaje de error visible');
-  }
+  // 2️⃣ validar DOM del home
+  await this.page.locator(this.selectorHome).waitFor({
+    state: 'visible',
+    timeout: 30000
+  });
 }
+
+
 
 
 }
